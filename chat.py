cd65c3ed9aeeaa660b0adebc966d7171fe93fd28
sakibalig <sakibalig2003@gmail.com>
2025-01-29 16:22:59 +0530

    manage the prompt logic

--- chat.py
+++ chat.py
@@ -3,25 +3,8 @@ import composio_autogen
 import autogen
 import composio
 import json
-import subprocess
 
-COMPOSIO_API_KEY = os.getenv("COMPOSIO_API_KEY", "")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
-GITHUB_ACCESS_TOKEN = os.getenv("GITHUB_ACCESS_TOKEN", "")
-
-clone_dir = "make a directory where you will clone this repository"
-
-GITHUB_USERNAME = input("Enter the name of the owner of the repo: ").strip()
-REPO_NAME = input("Enter the name of the repository: ").strip()
-
-repository_url = f"https://{GITHUB_ACCESS_TOKEN}@github.com/{GITHUB_USERNAME}/{REPO_NAME}.git"
-
-if not os.path.exists(clone_dir):
-    os.makedirs(clone_dir)
-subprocess.run(["git", "clone", repository_url, clone_dir], check=True)
-
-# Set the repo_dir to the cloned repository directory
-repo_dir = clone_dir
+repo_dir = "/Users/sakibkhan/Desktop/Assignments/Drizz33/pub-sub"
 
 toolset = composio_autogen.ComposioToolSet(
     metadata={
@@ -96,8 +79,9 @@ def ask_in_chat(message, response):
     return result
 
 
-# toolset.register_tools(apps=[composio_autogen.App.CODE_ANALYSIS_TOOL_GET_METHOD_BODY], caller=chatbot, executor=user_proxy)
-toolset.register_tools(actions=[composio_autogen.Action.FILETOOL_CHANGE_WORKING_DIRECTORY, composio_autogen.Action.FILETOOL_OPEN_FILE, composio_autogen.Action.FILETOOL_SCROLL, composio_autogen.Action.FILETOOL_SEARCH_WORD, composio_autogen.Action.FILETOOL_FIND_FILE, composio_autogen.Action.FILETOOL_LIST_FILES, composio_autogen.Action.CODE_ANALYSIS_TOOL_GET_METHOD_BODY, composio_autogen.Action.CODE_ANALYSIS_TOOL_GET_CLASS_INFO, composio_autogen.Action.CODE_ANALYSIS_TOOL_GET_RELEVANT_CODE, composio_autogen.Action.CODE_ANALYSIS_TOOL_GET_METHOD_SIGNATURE], caller=chatbot, executor=user_proxy)
+toolset.register_tools(apps=[composio_autogen.App.CODE_ANALYSIS_TOOL], caller=chatbot, executor=user_proxy)
+toolset.register_tools(actions=[composio_autogen.Action.FILETOOL_CHANGE_WORKING_DIRECTORY, composio_autogen.Action.FILETOOL_OPEN_FILE, composio_autogen.Action.FILETOOL_SCROLL,
+                       composio_autogen.Action.FILETOOL_EDIT_FILE, composio_autogen.Action.FILETOOL_SEARCH_WORD, composio_autogen.Action.FILETOOL_SEARCH_WORD, composio_autogen.Action.FILETOOL_FIND_FILE, composio_autogen.Action.FILETOOL_WRITE, composio_autogen.Action.FILETOOL_CREATE_FILE, composio_autogen.Action.FILETOOL_RENAME_FILE, composio_autogen.Action.FILETOOL_LIST_FILES], caller=chatbot, executor=user_proxy)
 
 
 question = """
@@ -106,16 +90,15 @@ You have access to the following tools to search the repo:
 - `CODE_ANALYSIS_TOOL_GET_CLASS_INFO`: Fetch information about a class in the repository.
 - `CODE_ANALYSIS_TOOL_GET_METHOD_BODY`: Fetch the body of a method in the repository.
 - `CODE_ANALYSIS_TOOL_GET_METHOD_SIGNATURE`: Fetch the signature of a method in the repository.
-- 'CODE_ANALYSIS_TOOL_GET_RELEVANT_CODE': Fetch the relevant code for a given query.
 - `FILETOOL_OPEN_FILE`: Open a file in the repository and view the contents (only 100 lines are displayed at a time).
 - `FILETOOL_SCROLL`: Scroll through a file in the repository.
 - `FILETOOL_SEARCH_WORD`: Search for a word in the repository.
-- `FILETOOL_FIND_FILE`: Find a file in the repository.
-
+- 'FILETOOL_CREATE_FILE': Create a new file in the repository.
+- 'FILETOOL_EDIT_FILE': Edit a file in the repository.
 
 Remember: 
 - This is an API service repository that would be running as a service.
-- Identify the main entry point of the service and list all the APIs that can be called once the service is running.
+- Identify the **main entry point** of the service and list all the APIs that can be called once the service is running.
 - Start by Provide a structured summary of each API, including:
   - The API endpoint.
   - The HTTP method (e.g., GET, POST).
@@ -135,16 +118,19 @@ You have access to the following tools to search the repo:
 - `FILETOOL_OPEN_FILE`: Open a file in the repository and view the contents (only 100 lines are displayed at a time).
 - `FILETOOL_SCROLL`: Scroll through a file in the repository.
 - `FILETOOL_SEARCH_WORD`: Search for a word in the repository.
+- 'FILETOOL_CREATE_FILE': Create a new file in the repository.
+- 'FILETOOL_EDIT_FILE': Edit a file in the repository.
 
-Note: analyse the code do not execute any code block. Just answer the questions .
-
-Call the provided functions as many times as necessary to answer the user's question. If you are unable to answer due to incomplete or unclear input, ask the user to "Please provide the question properly." If the input remains insufficient or the user does not respond, reply with "TERMINATE." Ensure that your answers are very detailed and specific to the user's query, addressing all aspects comprehensively. Once the question is fully addressed or the user input is empty, respond with "TERMINATE."
+Note: analyse the code do not execute any coe block. Just answer the questions.
+Call these functions as many times as needed to answer the question. And give very detailed and specific answer of the question When you are done or the user response is empty, reply TERMINATE.
 """
 
 question = question.format(repo_dir=repo_dir)
 
 with autogen.Cache.disk(cache_path_root=f"{os.getcwd()}+/cache") as cache:
     response = user_proxy.initiate_chat(chatbot, message=question, cache=cache)
+
+question = input("Enter the question: ")
 
 question = input("Enter the question: ")
 while question.lower() != "exit":
